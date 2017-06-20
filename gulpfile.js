4c468c75b4ba64de36bd356eaa45323dba7c84de
Josue MacBook Win <josuetec2003@icloud.com>
2017-06-20 13:54:13 -0600

    Reload server with nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const nodemon = require('gulp-nodemon')
 
 gulp.task('sass', () => {
     gulp
 	.src('scss/styles.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('public/css/')
+})
+
+gulp.task('reload', () => {
+    nodemon({
+	script: 'server.js'
+    })
 })
 
 gulp.task('default', ['sass'])
